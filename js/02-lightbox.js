f46624d03d0ac02c5068bcb68e01d30318655847
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-06-17 23:36:47 +0300

    add 1st part of 2nd task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(elements) {
+	return elements.map(({preview, original, description}) => `<li class="gallery__item">
+	<a class="gallery__link" href="${original}">
+	<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>
+	</li>`).join('');
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+gallery.addEventListener('click', onClick);
+
+function onClick(e) {
+	e.preventDefault();
+	if (e.target.nodeName !== 'IMG') return;
+
+	gallery.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape') gallery.close();
+	});
+
+//TODO дальше нужно или думать, или спать...)
+	const lightbox = new SimpleLightbox('.gallery a', {captionType: 'alt', captionDelay: 250});
+}
